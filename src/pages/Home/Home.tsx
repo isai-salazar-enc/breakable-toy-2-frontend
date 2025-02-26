6a9c626176a539d5fed0774ac572daf823ec8657
Diego Isai Salazar Rico <diego.salazar@encora.com>
2025-02-26 11:11:41 -0600

    React router and initial Home page added

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+// This page is the Login with spotify button
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to My Spotify App</h1>
+            <a href={'#'}>
+                <button className="btn-green">Login with Spotify</button>
+            </a>
+        </div>
+    )
+};
+
+export default Home;
